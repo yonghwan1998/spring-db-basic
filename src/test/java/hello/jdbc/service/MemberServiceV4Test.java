900221e161265a72d2fe2a44c1a835b79a5b9b02
yongss <bbl737898@gmail.com>
2025-09-09 16:06:20 +0900

    feat: JDBC 반복 문제 해결
    - JDBCTemplate 적용
    - 불필요 반복 코드 제거

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -1,10 +1,7 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV3;
-import hello.jdbc.repository.MemberRepositoryV4_1;
-import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -50,7 +47,8 @@ class MemberServiceV4Test {
 
         @Bean
         MemberRepository memberRepository() {
-            return new MemberRepositoryV4_2(dataSource);
+//            return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource);
         }
 
         @Bean
